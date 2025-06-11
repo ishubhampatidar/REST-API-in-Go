4d160cc089d79f92d7ef0ed806d440958cd36a25
ishubhampatidar <shubhampatidar0105@gmail.com>
2025-06-11 23:46:55 +0530

    created the server

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/ishubhampatidar/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to students API")
+	// load config
+	cfg := config.MustLoad()
+	// database setup
+	// router setup
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome to students api"))
+	})
+
+	// server setup
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Printf("Server started: %s", cfg.Addr)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("failed to start server")
+	}
+
 }
